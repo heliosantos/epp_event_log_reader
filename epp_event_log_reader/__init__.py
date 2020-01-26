88b373435bb22c7f3dce585e3549777f002b7de3
Hélio Santos <heliosantos99@gmail.com>
2020-01-26 20:24:00 +0000

    initial commit

---
+++ epp_event_log_reader/__init__.py
@@ -0,0 +1,4 @@
+from .epp_event_log_reader import EventLogEntry
+from .epp_event_log_reader import EventLogReader
+
+__all__ = ['EventLogEntry', 'EventLogReader']
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epp_event_log_reader',
+    version='1.0',
+    description='A library to parse .event files from Eggplant Performance test results.',
+    packages=find_packages(),
+)
