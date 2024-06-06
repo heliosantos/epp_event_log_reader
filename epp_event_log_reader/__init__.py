2afabca21439e53e7d2e64b6df6ad9e7a880ec45
Hélio Santos <heliosantos99@gmail.com>
2024-06-06 20:29:00 +0000

    allow reading all event logs from a test

--- epp_event_log_reader/__init__.py
+++ epp_event_log_reader/__init__.py
@@ -1,4 +1,4 @@
-from .epp_event_log_reader import EventLogEntry
-from .epp_event_log_reader import EventLogReader
+from .event_log_entry import EventLogEntry, EventLogEntryNode
+from .epp_event_test_reader import read_test_events, read_events_file
 
-__all__ = ['EventLogEntry', 'EventLogReader']
+__all__ = ['EventLogEntry', 'EventLogEntryNode', 'read_test_events', 'read_events_file']
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epp_event_log_reader',
-    version='1.0',
-    description='A library to parse .event files from Eggplant Performance test results.',
+    version='2.0',
+    description='An Eggplant Performance test results reader',
     packages=find_packages(),
 )
